cd98c340bd75a9e3cecdba2e42907ebbf582a3d6
Gergő Sábián <gergosabian@gmail.com>
2025-03-11 15:27:47 +0100

    Fix subnets

--- main.tf
+++ main.tf
@@ -24,7 +24,7 @@ module "blog_vpc" {
   name = "dev"
   cidr = "10.0.0.0/16"
 
-  azs             = ["us-west-2a"]
+  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
   tags = {
@@ -51,7 +51,7 @@ module "alb" {
 
   name    = "blog-alb"
   vpc_id  = module.blog_vpc.vpc_id
-  subnets = [module.blog_vpc.public_subnets[0]]
+  subnets = module.blog_vpc.public_subnets
   security_groups = [module.blog_sg.security_group_id]
 
   listeners = {
